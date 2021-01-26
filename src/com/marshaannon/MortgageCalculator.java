a84268d90a8e8da3386be40815f4a965ecaf6d35
Marsha Annon”git config --global user.name Marsha <marshaannon@gmail.com>
2021-01-25 19:02:00 -0500

    extract code from main and create class with calculate mortgage logic

--- src/com/marshaannon/MortgageCalculator.java
+++ src/com/marshaannon/MortgageCalculator.java
@@ -1,2 +1,41 @@
-package com.marshaannon;public class MortgageCalculator {
+package com.marshaannon;
+
+public class MortgageCalculator {
+
+    private int principal;
+    private float annualInterest;
+    private byte years;
+
+    public MortgageCalculator(int principal, float annualInterest, byte years) {
+        this.principal = principal;
+        this.annualInterest = annualInterest;
+        this.years = years;
+    }
+
+    public double calculateMortgage() {
+
+        float monthlyInterest = annualInterest / Main.PERCENT / Main.MONTHS_IN_YEAR;
+        short numberOfPayments = (short) (years * Main.MONTHS_IN_YEAR);
+
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        return mortgage;
+    }
+
+    public double calculateBalance(short numberOfPaymentsMade) {
+        float monthlyInterest = annualInterest / Main.PERCENT / Main.MONTHS_IN_YEAR;
+        short numberOfPayments = (short) (years * Main.MONTHS_IN_YEAR);
+
+        double balance = principal
+                * (Math.pow(1 + monthlyInterest, numberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        return balance;
+    }
+
+    public short getYears() {
+        return years;
+    }
 }
--- src/com/marshaannon/Print.java
+++
@@ -1,5 +0,0 @@
-package com.marshaannon;
-
-public class PrintMortgage {
-
-}
